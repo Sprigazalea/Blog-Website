a6f0b8a63a85c13c0c7bb228507265203ee7bf7a
Riley <dickersonevan82@gmail.com>
2025-02-18 17:11:55 -0500

    previews div

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
         <h1 className='font-semibold w-2xl m-0 italic'>Sprig's Blog</h1>
       </header>
       <p className='text-blue-600'>test</p>
-      <div id='previews'></div>
+      <div className='w-90% h-auto border border-solid border-white-600' id='previews'>test</div>
     </div>
   );
 }
