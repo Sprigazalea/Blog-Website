7e837c740d7d616c9e009765f0302aaf92864e85
Riley <dickersonevan82@gmail.com>
2025-02-18 16:20:32 -0500

    added header tag, div tag, h1 tag, with styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import './App.css'
 
 function App() {
   return (
-    <p className='text-blue-600'>test</p>
+    <div>
+      <header>
+        <h1 className='font-semibold w-24 h-24 border border-solid border-indigo-600 w-2xl'>Sprig's Blog</h1>
+      </header>
+      <p className='text-blue-600'>test</p>
+    </div>
   );
 }
 
