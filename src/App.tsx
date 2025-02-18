46ed456b7d97ba2e26f0f1ab82a2a9094f5a8b2a
Riley <dickersonevan82@gmail.com>
2025-02-18 16:41:29 -0500

    white border

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div>
-      <header className='flex items-center w-24 h-24 border border-solid border-indigo-600 w-auto'>
+      <header className='flex items-center w-24 h-24 border border-solid border-white-600 w-auto'>
         <h1 className='font-semibold w-2xl m-0 italic'>Sprig's Blog</h1>
       </header>
       <p className='text-blue-600'>test</p>
