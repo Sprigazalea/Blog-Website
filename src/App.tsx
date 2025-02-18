a48accf56e86bbdfe1d8e1105ff22aaa2294e5e8
Riley <dickersonevan82@gmail.com>
2025-02-18 16:29:12 -0500

    moved some styling to parent div

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
   return (
     <div>
-      <header>
-        <h1 className='font-semibold w-24 h-24 border border-solid border-indigo-600 w-2xl'>Sprig's Blog</h1>
+      <header className='flex items-center w-24 h-24 border border-solid border-indigo-600 w-auto'>
+        <h1 className='font-semibold w-2xl m-0 italic'>Sprig's Blog</h1>
       </header>
       <p className='text-blue-600'>test</p>
     </div>
