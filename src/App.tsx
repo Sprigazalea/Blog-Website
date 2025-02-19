b4765c833d93cd29179b5c444d78207009520cb3
Riley <dickersonevan82@gmail.com>
2025-02-18 20:41:29 -0500

    added ~ to h1

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <header className='flex items-center w-24 h-24 border border-solid border-white-600 w-auto'>
-        <h1 className='font-semibold w-2xl m-0 italic'>Sprig's Blog</h1>
+        <h1 className='font-semibold w-2xl m-0 italic'>~ Sprig's Blog ~</h1>
       </header>
       <p className='text-blue-600'>test</p>
       <div className='w-90% h-auto border border-solid border-white-600' id='previews'>test</div>
