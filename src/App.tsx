15cfded71424eaf0d01a9ef285dcecdfd96acf87
Riley <dickersonevan82@gmail.com>
2025-02-18 20:44:56 -0500

    removed button properties from css file

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ function App() {
         <h1 className='font-semibold w-2xl m-0 italic'>~ Sprig's Blog ~</h1>
       </header>
       <p className='text-blue-600'>test</p>
-      <div className='w-90% h-auto border border-solid border-white-600' id='previews'>test</div>
+      <div className='w-90% h-auto border border-solid border-white-600' id='previews'>
+        test
+      </div>
     </div>
   );
 }
