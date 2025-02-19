51180d47553075dbc09afc7f2846b3fe48076e3c
Riley <dickersonevan82@gmail.com>
2025-02-18 21:35:56 -0500

    added new div to push separate preview borders

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,30 @@ function App() {
       </header>
       <p className='text-blue-600'>test</p>
       <div className='w-90% h-auto border border-solid border-white-600' id='previews'>
-        <div className='flex p-4' id='preview-1'>
-          <img className='max-h-40' src='00048.png'></img>
-          <div>
-            <h2>Title</h2>
-            <p>Text</p>
-            <a>Tag</a>
+        <div className='p-4'>
+          <div className='flex p-4 border border-solid border-white-600' id='preview-1'>
+            <img className='max-h-40' src='00048.png'></img>
+            <div>
+              <h2>Title</h2>
+              <p>Text</p>
+              <a>Tag</a>
+            </div>
           </div>
-        </div>
-        <div className='flex p-4' id='preview-2'>
-          <img className='max-h-40' src='00048.png'></img>
-          <div>
-            <h2>Title</h2>
-            <p>Text</p>
-            <a>Tag</a>
+          <div className='flex p-4' id='preview-2'>
+            <img className='max-h-40' src='00048.png'></img>
+            <div>
+              <h2>Title</h2>
+              <p>Text</p>
+              <a>Tag</a>
+            </div>
           </div>
-        </div>
-        <div className='flex p-4' id='preview-3'>
-          <img className='max-h-40' src='00048.png'></img>
-          <div>
-            <h2>Title</h2>
-            <p>Text</p>
-            <a>Tag</a>
+          <div className='flex p-4' id='preview-3'>
+            <img className='max-h-40' src='00048.png'></img>
+            <div>
+              <h2>Title</h2>
+              <p>Text</p>
+              <a>Tag</a>
+            </div>
           </div>
         </div>
       </div>
